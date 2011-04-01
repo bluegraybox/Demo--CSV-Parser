c7f57180891bddd34aff562d921c8f842490b8c8
Colin MacDonald <colin@bluegraybox.com>
2011-04-01 14:30:14 -0400

    Moved RunningAverage into CSVParser.java.
    Since this is mostly for illustrating the code needed for Java v Ruby, it's nice to have it in one file.

--- java/src/main/java/com/bluegraybox/demo/CSVParser.java
+++ java/src/main/java/com/bluegraybox/demo/CSVParser.java
@@ -121,3 +121,18 @@ public class CSVParser {
 	}
 	
 }
+
+class RunningAverage {
+	private int count = 0;
+	private double average = 0;
+
+	void updateAverage(double value) {
+		double old_total = average * count;
+		average = (old_total + value) / (count + 1);
+		count += 1;
+	}
+
+	public double getValue() {
+		return average;
+	}
+}
